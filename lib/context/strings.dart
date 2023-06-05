60c8cc64fde286e1b98d7aef3bf04ca64dc82cc8
Nahiduzzaman <nahiduzzaman15-12736@gmail.com>
2023-06-06 00:55:03 +0600

    Custom Navigation and Usage Page Design

--- lib/context/strings.dart
+++ lib/context/strings.dart
@@ -4,19 +4,29 @@ import 'package:google_fonts/google_fonts.dart';
 var backGroundColor = const Color(0xff4C7380);
 var cardColor = const Color(0xffD8E4E8);
 var darkColor = const Color(0xff404040);
+var lightDarkColor = const Color(0xff878787);
+var lightGreenColor = const Color(0xff659A6E);
+var grayColor = const Color(0xff9A7265);
 
 var textWhite =GoogleFonts.poppins(fontSize: 12, color: Colors.white);
-var cardTextStyle= GoogleFonts.poppins(color: const Color(0xff404040),fontSize: 12,);
-var cardTextStyleBold= GoogleFonts.poppins(color: const Color(0xff404040),fontSize: 12,fontWeight: FontWeight.bold);
+var text =GoogleFonts.poppins(fontSize: 12, color: cardColor);
+var textLightDark =GoogleFonts.poppins(fontSize: 12, color: lightDarkColor);
+var textLightGreen =GoogleFonts.poppins(fontSize: 12, color: lightGreenColor);
+var textLightGray =GoogleFonts.poppins(fontSize: 12, color: grayColor);
+var cardTextStyle= GoogleFonts.poppins(color: darkColor,fontSize: 12,);
+var cardTextStyleBold= GoogleFonts.poppins(color: darkColor,fontSize: 12,fontWeight: FontWeight.bold);
 
 var tempColor = GoogleFonts.poppins(fontSize: 14,fontWeight: FontWeight.bold,color: Colors.white);
-var titleColor = GoogleFonts.poppins(fontSize: 14,fontWeight: FontWeight.bold,color: darkColor);
+var titleColorDark = GoogleFonts.poppins(fontSize: 14,fontWeight: FontWeight.bold,color: darkColor);
+var titleColorWhite = GoogleFonts.poppins(fontSize: 14,fontWeight: FontWeight.bold,color: cardColor);
 var tempColorLite = GoogleFonts.poppins(fontSize: 14,color: Colors.white);
-var nev = GoogleFonts.poppins(fontSize: 14,fontWeight: FontWeight.bold,color: backGroundColor);
+var textBackColorBold = GoogleFonts.poppins(fontSize: 14,fontWeight: FontWeight.bold,color: backGroundColor);
+var textBackColorLite = GoogleFonts.poppins(fontSize: 14,color: backGroundColor);
 
 var seeAll = GoogleFonts.poppins(fontSize: 16,fontWeight: FontWeight.bold,color: backGroundColor);
 var midTextLite = GoogleFonts.poppins(fontSize: 16,color: Colors.white);
-var midTextLBold = GoogleFonts.poppins(fontSize: 16,color: Colors.white,fontWeight: FontWeight.bold);
+var midWhiteTextBold = GoogleFonts.poppins(fontSize: 16,color: Colors.white,fontWeight: FontWeight.bold);
+var midText = GoogleFonts.poppins(fontSize: 16,color: cardColor,fontWeight: FontWeight.bold);
 
 var cardTextStyle1= GoogleFonts.poppins(fontWeight: FontWeight.bold,fontSize: 18,color: const Color(0xff404040),);
 var cardWhiteText= GoogleFonts.poppins(fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white);
@@ -26,3 +36,4 @@ var bigTitleText = GoogleFonts.poppins(fontSize: 50,fontWeight: FontWeight.bold,
 
 
 var decoration =BoxDecoration(color: backGroundColor, borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(58)));
+var decoration1 =const BoxDecoration(borderRadius: BorderRadius.only(topRight: Radius.circular(56),),color: Colors.white);
